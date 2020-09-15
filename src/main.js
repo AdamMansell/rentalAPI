3da9e9c49b0e451c2bd925511dac203b9dfd856e
AdamManselll <adammansellhmb@gmail.com>
2020-09-15 14:37:20 -0700

    Update webpage to allow map functionality, and name print to screen.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+// import { prototype } from 'html-webpack-plugin';
 
-
-$(document).ready(function() {
-  $('#search').submit(function(event) {
-    event.preventDefault();
+$(document).ready(function () {
+  $("#saintJohns").click(function () {
+    $("#neighborhood").text($("#saintJohns").attr("value"));
+    $(".cityInfo").fadeIn(1000);
   });
+  $("#forestPark").click(function () {
+    $("#neighborhood").text($("#forestPark").attr("value"));
+    $(".cityInfo").fadeIn(1000);
+  });
+
+
+  // $('#search').submit(function (event) {
+  //   event.preventDefault();
+
+  //   function printName(name) {
+  // $("#neighborhood").text(name);
+  // $(".cityInfo").show();
+  //   }
+
+  //   printName(name);
+
+  // });
 });
