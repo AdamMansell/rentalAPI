12e2463f3bea71ef1a04f2d3ba6ec60ea5fc905c
Forrest Reiland <you@example.com>
2020-09-18 17:40:41 -0700

    test gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-
+const productionConfig = merge([
+  
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/webpack-demo/", 
   },
   devtool: 'eval-source-map',
   devServer: {               
